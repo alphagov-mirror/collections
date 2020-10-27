a585e218837da808a1d51f74a3ce30ab084631f3
Chris Yoong <chris.yoong@digital.cabinet-office.gov.uk>
2020-10-27 09:23:05 +0000

    Adjusting fallback copy
    When cookies are not accepted adjusting Content to ensure that screen-readers do not read the YouTube fallback text in a disjointed manner. Maintaining existing Design

--- app/assets/javascripts/modules/youtube-player.js
+++ app/assets/javascripts/modules/youtube-player.js
@@ -7,7 +7,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   Modules.YouTubePlayer = function () {
     if (window.GOVUK.cookie('global_bar_seen')) {
 
-      if (window.location.pathname === '/coronavirus' || window.location.pathname === '/transition') {
+      if (window.location.pathname === '/coronavirus' || '/transition') {
         // Provide CSS hook for when cookies are accepted.
         let youTubeLogo = document.querySelector(".youtube__video-wrapper");
         youTubeLogo.classList.add("youtube__video-wrapper--cookies-accepted");
